9a805e8cd8f4240a66d1f4f1cd4c748fd6695d69
Campbell McGavin <campbellmcgavin@gmail.com>
2023-03-07 22:25:22 -0700

    Initial Commit

---
+++ Envisionary/EnvisionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvisionaryApp.swift
+//  Envisionary
+//
+//  Created by Campbell McGavin on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EnvisionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
